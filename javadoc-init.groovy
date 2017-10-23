78355b827ec9fab36d5fa3f41ab5a4786732000b
fgiff <ffgiff@gmail.com>
2017-10-23 19:19:43 +0100

    Update javadoc script
    Ignore Kotlin source files.
    Don't extract test code.
    Remove reference to missing property. This might break multi-project
    doc builds.

--- javadoc-init.groovy
+++ javadoc-init.groovy
@@ -28,21 +28,22 @@ final class JavadocHelper {
                               group:'Documentation',
                               description:'Generates documentation using javadoc tool.',
                               dependsOn:[project.assembleRelease],]) {
-            classpath = project.configurations.compile +
-                        project.configurations.testCompile +
-                        project.configurations.androidTestCompile +
-                        project.files(project.android.bootClasspath)
-//            classpath = project.configurations.compile + files(android.bootClasspath)
+//            classpath = project.configurations.compile +
+//                        project.configurations.testCompile +
+//                        project.configurations.androidTestCompile +
+//                        project.files(project.android.bootClasspath)
+            classpath = project.configurations.compile + project.files(project.android.bootClasspath)
             for (final String variantType : [APP_VARIANTS, LIB_VARIANTS]) {
                 if (project.android.hasProperty(variantType)) {
                     classpath += getClasspathForVariantType(variantType, project)
                 }
             }
             exclude '**/*.aj'
-            source = [project.android.sourceSets.main.java.srcDirs,
-                      project.android.sourceSets.androidTest.java.srcDirs,
-                      project.android.sourceSets.test.java.srcDirs,]
-//            source = [android.sourceSets.androidTest.java.srcDirs]
+            exclude '**/*.kt'
+//            source = [project.android.sourceSets.main.java.srcDirs,
+//                      project.android.sourceSets.androidTest.java.srcDirs,
+//                      project.android.sourceSets.test.java.srcDirs,]
+            source = [project.android.sourceSets.main.java.srcDirs]
             options.setLinksOffline([new JavadocOfflineLink(
                     'https://developer.android.com/reference',
                     project.android.sdkDirectory.absolutePath
@@ -57,11 +58,7 @@ final class JavadocHelper {
         project.android."${variantType}".all { variant ->
             if (variant.buildType.name == 'release') {
                 classpath += project.files("${variant.javaCompile.destinationDir}")
-                if ("${variantType}" == APP_VARIANTS) {
-                    variant.compileLibraries.each { lib ->
-                        classpath += project.files(lib)
-                    }
-                } else if ("${variantType}" == LIB_VARIANTS) {
+                if ("${variantType}" == APP_VARIANTS || "${variantType}" == LIB_VARIANTS) {
                     classpath += variant.javaCompile.classpath
                 }
             }
